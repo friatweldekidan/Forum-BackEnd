b1c4614b1965b02647e350ff2226d66727377555
friatweldekidan <34029637+friatweldekidan@users.noreply.github.com>
2023-05-12 15:57:48 +0300

    database deployed

--- server/config/database.js
+++ server/config/database.js
@@ -1,33 +1,31 @@
-const mysql = require('mysql')
+const mysql = require("mysql2");
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.MYSQL_DB,
-   connectionLimit: 10
-  });
-   
-  pool.getConnection(()=>{
-      console.log('database connected');
-  });
+// const pool = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.MYSQL_DB,
+//    connectionLimit: 10
+//   });
 
-  let registration = `CREATE TABLE if not exists registration(
+const pool = mysql.createConnection(process.env.DATABASE_URL);
+
+let registration = `CREATE TABLE if not exists registration(
     user_id int auto_increment,
     user_name varchar(255) not null,
     user_email varchar(255) not null,
     user_password varchar(255) not null,
     PRIMARY KEY (user_id)
   )`;
-  let profile = `CREATE TABLE if not exists profile(
+let profile = `CREATE TABLE if not exists profile(
     user_profile_id int auto_increment,
     user_id int not null,
     first_name varchar(255) not null,
     last_name varchar(255) not null,
-    PRIMARY KEY (user_profile_id),
-    FOREIGN KEY (user_id) REFERENCES registration(user_id)
+    PRIMARY KEY (user_profile_id)
+
   )`;
-  let question = `CREATE TABLE if not exists question(
+let question = `CREATE TABLE if not exists question(
     question_id int auto_increment,
     question varchar(255) not null,
     question_description varchar(255),
@@ -36,39 +34,42 @@ const pool = mysql.createPool({
     post_id varchar(255) not null,
     user_id int not null,
     PRIMARY KEY (question_id),
-    UNIQUE KEY (post_id),
-    FOREIGN KEY (user_id) REFERENCES registration(user_id)
+    UNIQUE KEY (post_id)
+   
   )`;
-  let answer = `CREATE TABLE if not exists answer(
+let answer = `CREATE TABLE if not exists answer(
     answer_id int auto_increment,
     answer varchar(255) not null,
     answer_code_block varchar(255),
     user_id int not null,
     question_id int not null,
-    PRIMARY KEY (answer_id),
-    FOREIGN KEY (user_id) REFERENCES registration(user_id),
-    FOREIGN KEY (question_id) REFERENCES question(question_id)
+    PRIMARY KEY (answer_id)
+   
   )`;
 
+pool.query(registration, (err, results) => {
+  if (err) {
+    throw err;
+  }
+  console.log("registration table created");
+});
+pool.query(profile, (err, results) => {
+  if (err) {
+    throw err;
+  }
+  console.log("profile table created");
+});
+pool.query(question, (err, results) => {
+  if (err) {
+    throw err;
+  }
+  console.log("question table created");
+});
+pool.query(answer, (err, results) => {
+  if (err) {
+    throw err;
+  }
+  console.log("answer table created");
+});
 
-
-  pool.query(registration, (err, results) => {
-    if (err){throw err;}  
-    console.log('registration table created');
-  })
-  pool.query(profile, (err, results) => {
-    if (err){throw err;}  
-    console.log('profile table created');
-  })
-  pool.query(question, (err, results) => {
-    if (err){throw err;}  
-    console.log('question table created');
-  })
-  pool.query(answer, (err, results) => {
-    if (err){throw err;}  
-    console.log('answer table created');
-  })
-
-
-
-  module.exports = pool;
+module.exports = pool;
